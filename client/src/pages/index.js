0920fef2f15845d974e086cba1421fd1d0165132
Aninda13 <50640239+Aninda13@users.noreply.github.com>
2020-11-24 06:59:21 -0500

    Added Login/Reg page with updated checkbox

---
+++ client/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const MainPage = () => {
+    return(
+        <div>
+            <h1>This is a router test for main Page</h1>
+            <h3>Main Page</h3>
+        </div>
+    )
+};
+
+export default MainPage;
